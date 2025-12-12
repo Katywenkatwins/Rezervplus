f2a17dc80f22df8d9628ace9e58a0355f5f3c187
Kate Smijan <104097704+Katywenkatwins@users.noreply.github.com>
2025-12-12 09:03:47 +0200

    Update IdCard.tsx

--- src/components/IdCard.tsx
+++ src/components/IdCard.tsx
@@ -35,7 +35,7 @@ export function IdCard({ userData, onPlusClick, onPhotoChange }: IdCardProps) {
   };
 
   return (
-    <div className="perspective-[1000px] w-[347px] h-[490px]  mr-[0px] mb-[0px] ml-[0px]">
+    <div className="perspective-[1000px] w-[347px] h-[490px] mt-[80px] mr-[0px] mb-[0px] ml-[0px]">
       <motion.div
         className="relative w-full h-full"
         style={{ transformStyle: 'preserve-3d' }}
